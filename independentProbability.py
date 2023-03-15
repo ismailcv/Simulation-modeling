1d8b0df99bc7cebf7d57938002c552aec2864b24
İsmail Can Varlı <icanvarli@outlook.com>
2023-03-15 23:55:35 +0300

    ilk 2 ödev

---
+++ independentProbability.py
@@ -0,0 +1,55 @@
+#2. soru bağımsız olasılık sorusu
+#İsmail Can Varlı    171420004
+
+# random modülünü import ediyoruz
+import random
+
+# kullanıcıdan borunun uzunluğu, canlının tek seferde ilerleyebileceği uzunluk ve kaç kere hareket edeceği bilgilerini alıyoruz
+a = int(input("Borunun uzunluğu: "))
+x = int(input("Canlının tek seferde ilerleyebileceği uzunluk: "))
+y = int(input("Kaç kere hareket edecek: "))
+
+# canlı başlangıçta borunun ortasında bulunuyor
+n = a // 2
+
+# kullanıcının girdiği değerleri yazdırıyoruz
+print(f"\nBorunun uzunluğu: {a}, Canlının tek seferde ilerleyebileceği uzunluk: {x}, Kaç kere hareket edecek: {y}\n")
+
+# simülasyon sayısı ve toplam uzaklık değişkenlerini tanımlıyoruz
+num_simulations = 10
+total_distance = 0
+
+# belirlenen sayıda simülasyon yapmak için for döngüsü kullanıyoruz
+for sim in range(num_simulations):
+    # her simülasyon başlangıcında canlı borunun ortasında bulunuyor
+    n = a // 2
+    
+    # simülasyonun başladığına dair bir mesaj yazdırıyoruz
+    print(f"Simülasyon {sim+1} başlıyor...")
+    
+    # belirtilen sayıda adım atmak için for döngüsü kullanıyoruz
+    for i in range(y):
+        # canlı borunun sonuna veya başına ulaştığında, diğer tarafından devam etmesi için işlem yapılıyor
+        if n <= 0:
+            n += a
+        elif n >= a:
+            n -= a
+
+        # canlının hangi yönde hareket edeceğini rastgele seçiyoruz
+        direction = random.choice([-1, 1])
+        n += direction * x
+
+        # canlının yeni konumunu ve hangi yöne gittiğini yazdırıyoruz
+        print(f"{i+1}. konum: {n}, {'' if direction == 1 else 'sol'} tarafa gidiyor")
+
+    # canlının başlangıç noktasına olan uzaklığını hesaplıyoruz
+    distance = abs(n - a//2)
+    total_distance += distance
+
+    # simülasyon sonucunu yazdırıyoruz
+    print(f"\nSimülasyon {sim+1} tamamlandı. Başlangıç noktasına olan uzaklık: {distance}\n")
+
+# simülasyonların ortalamasını hesaplayıp yazdırıyoruz
+average_distance = total_distance / num_simulations
+
+print(f"Simülasyonların ortalaması: {average_distance}")
---
+++ monteCarloSimulation.py
@@ -0,0 +1,51 @@
+# monte karlo yöntemi ile verilen problemin çözümü 
+#İsmail Can VARLI   171420004 
+import random
+
+def monte_carlo_triangle_area(base: float, height: float, num_samples: int) -> float:
+    num_points_inside = 0
+    for i in range(num_samples):
+        x = random.uniform(0, base)
+        y = random.uniform(0, height)
+        if y <= -height / base * x + height:
+            num_points_inside += 1
+    area_rectangular = base * height
+    area_triangle = area_rectangular * num_points_inside / num_samples
+    return area_triangle
+
+base1 = 2  # taban uzunluğu
+height1 = 2  # yükseklik
+base2 = 4  # taban uzunluğu
+height2 = 1  # yükseklik
+base3 = 2  # taban uzunluğu
+height3 = 1  # yükseklik
+
+num_samples = 1000000  # nokta sayısı
+
+triangle_area_1 = monte_carlo_triangle_area(base1, height1, num_samples)
+triangle_area_2 = monte_carlo_triangle_area(base2, height2, num_samples)
+triangle_area_3 = monte_carlo_triangle_area(base3, height3, num_samples)
+
+def monte_carlo_rectangle_area(length: float, width: float, num_samples: int) -> float:
+    num_points_inside = 0
+    for i in range(num_samples):
+        x = random.uniform(0, length)
+        y = random.uniform(0, width)
+        if x <= length and y <= width:
+            num_points_inside += 1
+    area_rectangular = length * width
+    area_rectangle = area_rectangular * num_points_inside / num_samples
+    return area_rectangle
+
+length1 = 4  # uzun kenar uzunluğu
+width1 = 1  # kısa kenar uzunluğu
+
+
+num_samples = 1000000  # nokta sayısı
+
+rectangle_area1 = monte_carlo_rectangle_area(length1, width1, num_samples)
+
+
+#Tüm alanlar toplanıyor ve sonucu bize veriyor.
+
+print(triangle_area_1 + triangle_area_2 + triangle_area_3 + rectangle_area1)
